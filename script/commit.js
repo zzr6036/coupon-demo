f912d3f5e96d52fa13a26f9c3fef878613637c1f
Ziru <zzrchirstina@gmail.com>
2022-01-03 22:40:17 +0800

    update sctipt

--- script/commit.js
+++ script/commit.js
@@ -1,10 +1,10 @@
-const path = require('path')
 
-console.log('path==>', path)
+
 const commits = process.argv.slice(2)
-console.log('commits====>', commits)
+// console.log('commits====>', commits)
 async function commit() {
     console.log('todo')
+    console.log('commits====>', commits)
 }
 
 commit()
