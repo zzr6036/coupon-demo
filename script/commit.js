91552432e73af99559888df713a168ad3535d355
Ziru <zzrchirstina@gmail.com>
2022-01-03 22:43:16 +0800

    JSON parse commits

--- script/commit.js
+++ script/commit.js
@@ -4,7 +4,7 @@ const commits = process.argv.slice(2)
 // console.log('commits====>', commits)
 async function commit() {
     console.log('todo')
-    console.log('commits====>', commits)
+    console.log('commits====>', JSON.parse(commits))
 }
 
 commit()
