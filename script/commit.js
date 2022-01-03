65223b66925bb7c0c417c72407b01f9031c93724
Ziru <zzrchirstina@gmail.com>
2022-01-03 22:34:48 +0800

    add process

--- script/commit.js
+++ script/commit.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
 console.log('path==>', path)
+const commits = process.argv.slice(2)
+console.log('commits====>', commits)
 async function commit() {
     console.log('todo')
 }
