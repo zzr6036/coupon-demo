39701f8ff6e7a2a83ddfc5007bc55823a2f41939
zzr6036 <zzrchirstina@gmail.com>
2022-01-03 17:58:01 +0800

    Update index.js
    test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import styles from '../../../styles/Header.module.css'
 const Header = props => {
     return (
         <div className={styles.header}>
-            Welcome to My Coupon Application
+            Welcome to My Coupon Application-1
         </div>
     )
 }
