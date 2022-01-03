473b1e20b01b4604421a3f4a4c1f205aa30867ae
Ziru <zzrchirstina@gmail.com>
2022-01-03 22:48:44 +0800

    test 1

--- src/components/Coupon/index.js
+++ src/components/Coupon/index.js
@@ -223,7 +223,7 @@ const Coupon = props => {
     }
 
     const renderTable = () => {
-        console.log('11')
+        console.log('1')
         console.log('22')
         console.log('33')
         if (coupons && coupons.length > 0) {
