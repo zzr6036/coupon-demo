9c80ad26a784ede2bc534d5ca0e54c73f5bf5453
Ziru <zzrchirstina@gmail.com>
2022-01-03 22:49:23 +0800

    test 3

--- src/components/Coupon/index.js
+++ src/components/Coupon/index.js
@@ -225,7 +225,7 @@ const Coupon = props => {
     const renderTable = () => {
         console.log('1')
         console.log('2')
-        console.log('33')
+        console.log('3')
         if (coupons && coupons.length > 0) {
             return (
                 <Table
