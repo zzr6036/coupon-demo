562fec1103d0a2e221dea1981497bd843c166985
Ziru <zzrchirstina@gmail.com>
2022-01-04 23:24:09 +0800

    change to es6

--- script/commit.js
+++ script/commit.js
@@ -1,4 +1,4 @@
-const { default: fetch } = require('node-fetch')
+import { default as fetch } from 'node-fetch'
 
 const commits = process.argv.slice(2)
 console.log('argu====>', process.argv)
@@ -11,11 +11,11 @@ async function commit() {
     // commit2()
 }
 
-// async function commit2() {
-//     const fetch2 = new fetch({ auth: `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` });
-//     const result2 = await fetch2('https://github.com/zzr6036/coupon-demo/commits')
-//     console.log('result2===>', result2)
-// }
+async function commit2() {
+    const fetch2 = new fetch({ auth: `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` });
+    const result2 = await fetch2('https://github.com/zzr6036/coupon-demo/commits')
+    console.log('result2===>', result2)
+}
 
 commit()
 
