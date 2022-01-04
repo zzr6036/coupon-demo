dd091bd315abe1e6e4e4547b45a7148e0d68148f
Ziru <zzrchirstina@gmail.com>
2022-01-04 23:41:25 +0800

    try

--- script/commit.js
+++ script/commit.js
@@ -1,5 +1,5 @@
-import { default as fetch } from 'node-fetch'
-// const { default: fetch } = require('node-fetch')
+// import { default as fetch } from 'node-fetch'
+const { default: fetch } = require('node-fetch')
 
 const commits = process.argv.slice(2)
 console.log('argu====>', process.argv)
