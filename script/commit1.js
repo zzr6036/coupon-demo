6051e1e7a4637ceb59ef109e1c7298d805a7cd27
zzr6036 <zzrchirstina@gmail.com>
2022-02-17 21:44:43 +0800

    [MH-123] This is the testing (#28)
    * this is the first commit
    
    * this is the second commit

--- script/commit1.js
+++ script/commit1.js
@@ -13,13 +13,15 @@ function commit() {
 
 function getTicketInfo(commitList, headCommit) {
     const titleResult = jiraTicketRegex.exec(headCommit)
+    const jiraTicket = titleResult[0]
     const isSkipDanger = headCommit.toLowerCase().includes('#skip-danger')
     let messageObj = commitList.reduce((acc, item) => {
-        if (jiraTicketRegex.exec(item)) {
-            return { ...acc, [jiraTicketRegex.exec(item)[0]]: [...([item] || [])] }
+        if (item.includes(jiraTicket)) {
+            return { ...acc, [jiraTicket]: [...([item] || [])] }
         }
     }, {})
-    // console.log('titleResult===>', titleResult)
+    console.log('jiraTicket===>', jiraTicket)
+    console.log('isSkipDanger===>', isSkipDanger)
     console.log('messageObj===>', messageObj)
     return {
         title: '',
@@ -45,3 +47,11 @@ commit()
 
 // node commit1.js "[MH-111] tHIS IS THE COMMIT (#26)" 
 // node commit1.js "[MH-111] tHIS IS THE COMMIT (#26) \n*[MH-111] test" 
+
+// '[MH-1111] This is the testing title (#27)\n' +
+// '\n' +
+// '* this is the first commit\r\n' +
+// '\r\n' +
+// '* this is the second commit'
+
+// node commit1.js '[MH-1111] This is the testing title (#27)\n \n * this is the first commit\r\n \r\n * this is the second commit'
