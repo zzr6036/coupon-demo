753f48acd9a9e773bd095f12b7f3274035e888f5
Ziru <zzrchirstina@gmail.com>
2022-02-24 10:47:51 +0800

    test author name

--- script/commit1.js
+++ script/commit1.js
@@ -1,6 +1,6 @@
 
 const headCommit = String(process.argv.slice(2))
-const jiraTicketRegex = /((SANFRANSOK|MH)-\d+)/
+const jiraTicketRegex = /((SANFRANSOK|MH|sanfransok|mh)-\d+)/g
 const sampleList = [
     '[MH-123] This is the testing (#28)',
     '',
@@ -8,54 +8,37 @@ const sampleList = [
     '',
     '* this is the second commit'
 ]
+
 function commit() {
-    const commitList = typeof headCommit === 'string' ? headCommit.split(/\r?\n/) : []
-    const { title, message } = getTicketInfo(commitList, headCommit)
+
+    const { title, message } = getTicketInfo(headCommit)
     // console.log('process.argv.slice===>', process.argv.slice(2))
-    // console.log(' process.argv ====>', process.argv)
+    console.log(' process.argv ====>', process.argv)
     // console.log('headCommit==>', headCommit)
     // console.log('commitList==>', commitList)
     console.log('title==>', title)
     console.log('message==>', message)
 }
 
-function getTicketInfo(commitList, headCommit) {
-    const titleResult = jiraTicketRegex.exec(headCommit)
-    const jiraTicket = titleResult[0]
+function getTicketInfo(headCommit) {
+    // const commitList = typeof headCommit === 'string' ? headCommit.split(/\r?\n/) : []
+    // const strHeadCommit = String(headCommit)
+    const commitList = headCommit.split(/\r\n|\n\r|\n|\r|\\r\n|\\n\r|\\r|\\n/) || []
+    let ticketList = headCommit.match(jiraTicketRegex) || []
+    ticketList = ticketList.map(x => typeof x === 'string' ? x.toUpperCase() : x)
+    const uniqTicketList = [...new Set(ticketList)];
+    let ticket = '';
+    uniqTicketList.forEach(item => {
+        ticket += `[${item}](http://xx/${item})  `;
+    })
     const message = commitList[0]
-    const isSkipDanger = headCommit.toLowerCase().includes('#skip-danger')
-    // let messageObj = commitList.reduce((acc, item) => {
-    //     if (item.includes(jiraTicket)) {
-    //         return { ...acc, [jiraTicket]: [...([item] || [])] }
-    //     }
-    // }, {})
-    // let messageObj = sampleList.reduce((acc, item) => {
-    //     if (item.includes(jiraTicket)) {
-    //         console.log('here')
-    //         return { ...acc, [jiraTicket]: [...([item] || [])] }
-    //     }
-
-    // }, {})
-    // console.log('jiraTicket===>', jiraTicket)
-    // console.log('isSkipDanger===>', isSkipDanger)
-    // console.log('messageObj===>', messageObj)
-    if (isSkipDanger) {
-        return {
-            title: '',
-            message: message
-        }
-    }
+    console.log('1. headCommit==>', headCommit)
+    console.log('2. commitList===>', commitList)
     return {
-        title: jiraTicket,
-        message: message
+        title: ticket ? `Jira Ticket: ${ticket}` : '',
+        message
     }
 }
-
-// async function commit2() {
-//     const fetch2 = new fetch({ auth: `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` });
-//     const result2 = await fetch2('https://github.com/zzr6036/coupon-demo/commits')
-//     console.log('result2===>', result2)
-// }
 
 commit()
 // this is test4
@@ -69,7 +52,7 @@ commit()
 // });
 
 // node commit1.js "[MH-111] tHIS IS THE COMMIT (#26)" 
-// node commit1.js "[MH-123] tHIS IS THE COMMIT (#26) \n*[MH-123] test" 
+// node commit1.js "[MH-123][SANFRANSOK-1234] tHIS IS THE COMMIT (#26) \n*[MH-123] test" 
 
 // '[MH-1111] This is the testing title (#27)\n' +
 // '\n' +
@@ -78,3 +61,5 @@ commit()
 // '* this is the second commit'
 
 // node commit1.js '[MH-1111] This is the testing title (#27)\n \n * this is the first commit\r\n \r\n * this is the second commit'
+// node commit1.js '[MH-1111][mh-123][MH-123] This is the testing title (#27)\n \n * this is the first commit\r\n \r\n * this is the second commit'
+// node commit1.js 'This is the testing title (#27)#skip-danger\n \n * this is the first commit\r\n \r\n * this is the second commit'
