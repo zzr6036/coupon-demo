b9d278f95f1e0d7b80ed5f40075883a029847b67
Ziru <zzrchirstina@gmail.com>
2023-03-01 22:08:57 +0800

    m2

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,9 +1,13 @@
-import { danger, markdown } from 'danger'
+import { danger, markdown, warn } from 'danger'
 const { generateReport } = require('./script/report')
 const pr = danger.github.pr
 
 if (pr) {
-    const data = generateReport()
-    console.log('1. pr=>', pr)
-    console.log('2. data=>', data)
+    if (pr.assignee === null) {
+        warn("Please assign someone to merge this PR, and optionally include people who should review.")
+        markdown('Please assign someone to merge this PR')
+    }
+    // const data = generateReport()
+    // console.log('1. pr=>', pr)
+    // console.log('2. data=>', data)
 }
