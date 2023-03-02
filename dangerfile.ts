2053b7a8f3c0b77fd38d68741902af486c1f0107
Ziru <zzrchirstina@gmail.com>
2023-03-02 10:57:48 +0800

    test

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,4 @@
-import { danger, markdown, warn } from 'danger'
+import { danger, markdown, warn, schedule } from 'danger'
 const { generateReport } = require('./script/report')
 const pr = danger.github.pr
 
@@ -8,8 +8,24 @@ if (pr) {
         markdown('Please assign someone to merge this PR')
     }
 
-    const data = generateReport()
+    const reportList = generateReport()
     console.log('1. pr=>', pr)
-    console.log('2. data=>', data)
+    console.log('2. reportList=>', reportList)
+
+    if (Object.keys(reportList).length) {
+        markdown('### Unit test coverage report:')
+        const coverReportRows: string[] = []
+        schedule(async () => {
+            Object.keys(reportList).forEach(key => {
+                coverReportRows.push(`| ${key} | ${reportList[key].statements.c} | ${reportList[key].branches.c} | ${reportList[key].functions.c} | ${reportList[key].lines.c} |`)
+            })
+            if (coverReportRows.length) {
+                markdown(
+                    '\n | Packages | Statements | Branches | Functions | Lines |\n | --- | --- | --- | --- | --- |\n' + coverReportRows.join('\n')
+                )
+            }
+        })
+    }
     // markdown(`This is test\n${data.join(', ')}`)
+
 }
