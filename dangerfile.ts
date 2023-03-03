2fd3df755bb3974de3124e4a945c0f5a80b3decc
zzr6036 <zzrchirstina@gmail.com>
2023-03-03 14:32:38 +0800

    test4 (#36)
    * m1
    
    * m2
    
    * m3
    
    * m3
    
    * m5
    
    * m5
    
    * m6
    
    * m6
    
    * m6
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test2
    
    * test2

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,10 +1,58 @@
-import { danger, markdown } from 'danger'
+import { danger, markdown, warn, schedule } from 'danger'
 const { generateReport } = require('./script/report')
+// import { generateReport } from './script/report'
+
 const pr = danger.github.pr
+const modifiedFiles = danger.git.modified_files;
+const isPRMerged = pr.merged
 
 if (pr) {
-    const data = generateReport()
-    console.log('1. pr=>', pr)
-    console.log('2. data=>', data)
-    markdown(`This is test\n${data.join(', ')}`)
+    // 1) No assigner
+    if (pr.assignee === null) {
+        warn("Please assign someone to merge this PR, and optionally include people who should review.")
+        markdown('Please assign someone to merge this PR')
+    }
+
+    // 2) If there are changes, but not unit test
+    if (!isPRMerged) {
+        const hasPackageChanges = modifiedFiles.length > 0;
+        const noUnitTestFiles = [];
+        const typescriptFilePattern = /.ts|.tsx/
+        const ignoreFiles = [/*'dangerfile.ts'*/]
+        modifiedFiles.filter(filepath => {
+            const pieces = filepath.split('/');
+            const lastPieces = pieces.pop();
+            const fileName = lastPieces?.split('.')[0];
+            const isIncludeIgnoreFiles = ignoreFiles.some(f => lastPieces.includes(f))
+            if (!isIncludeIgnoreFiles && typescriptFilePattern.test(lastPieces) && !filepath.includes(`${fileName}.test`)) {
+                noUnitTestFiles.push(filepath);
+            }
+        });
+        if (hasPackageChanges && noUnitTestFiles.length) {
+            markdown('### No unit test files : \n - ' + noUnitTestFiles.join('\n-'));
+        }
+    }
+
+
+    // 3) Generate report
+    if (isPRMerged) {
+        const reportList = generateReport()
+        console.log('1. pr=>', pr)
+        console.log('2. reportList=>', reportList)
+        if (Object.keys(reportList).length) {
+            markdown('### Unit test coverage report:')
+            const coverReportRows = []
+            schedule(async () => {
+                Object.keys(reportList).forEach(key => {
+                    coverReportRows.push(`| ${key} | ${reportList[key].statements.d || '-'} (${reportList[key].statements.e || '-'}) | ${reportList[key].branches.d || '-'} (${reportList[key].branches.e || '-'}) | ${reportList[key].functions.d || '-'} (${reportList[key].functions.e || '-'}) | ${reportList[key].lines.d || '-'} (${reportList[key].lines.e || '-'}) |`)
+                })
+                console.log('coverReportRows=>', coverReportRows)
+                if (coverReportRows.length > 0) {
+                    markdown(
+                        '\n | Packages | Statements | Branches | Functions | Lines |\n | --- | --- | --- | --- | --- |\n' + coverReportRows.join('\n')
+                    )
+                }
+            })
+        }
+    }
 }
--- script/report.js
+++ script/report.js
@@ -30,6 +30,96 @@ function generateReport() {
                 d: '50%',
                 e: '100/200'
             }
+        },
+        packageB: {
+            statements: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            },
+            branches: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            },
+            functions: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            },
+            lines: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            }
+        },
+        packageC: {
+            statements: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            },
+            branches: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            },
+            functions: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            },
+            lines: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            }
+        },
+        packageD: {
+            statements: {
+                a: null,
+                b: null,
+                c: null,
+                d: '',
+                e: ''
+            },
+            branches: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            },
+            functions: {
+                a: null,
+                b: null,
+                c: null,
+                d: '',
+                e: ''
+            },
+            lines: {
+                a: 100,
+                b: 100,
+                c: 200,
+                d: '50%',
+                e: '100/200'
+            }
         }
     }
     return report
