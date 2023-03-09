eaaabdaad42488be826e5f634c197da27fda6e3d
Ziru <zzrchirstina@gmail.com>
2023-03-09 10:41:01 +0800

    t

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,7 +2,7 @@ import { danger, markdown, warn, schedule } from 'danger'
 const { generateReport } = require('./script/report')
 // import { generateReport } from './script/report'
 // import defineConfig from './vitest.config'
-const { defineConfig } = require('./vitest.config')
+// const { defineConfig } = require('./vitest.config')
 
 const pr = danger.github.pr
 const modifiedFiles = danger.git.modified_files;
@@ -35,10 +35,9 @@ if (pr) {
             }
         });
         if (hasPackageChanges && noUnitTestFiles.length) {
-            markdown('### No unit test files : \n - ' + noUnitTestFiles.join('\n-'));
+            markdown('### No unit test files : \n - ' + noUnitTestFiles.join('\n- '));
         }
     }
-    console.log('defineConfig=>', defineConfig)
 
     // 3) Generate report
     // if (!isPRMerged) {
---
+++ src/components/FooterBackup/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../../styles/Footer.module.css'
+
+const Footer = props => {
+    return (
+        <div className={styles.footer}>
+            Coupon Application ©2021 Created by ZR
+        </div>
+    )
+}
+
+export default Footer
